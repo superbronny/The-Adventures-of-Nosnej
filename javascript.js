be205bce48b3b3833e1bd6674ac44d026f130b8e
superbronny <brontevthomson@gmail.com>
2024-08-12 12:24:38 +1000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+ });
+ 
